4e05f623d657f3e2707305bd6640ee3bd0497e2e
Ravindra-Pagidala <pagidalaravindra27@gmail.com>
2026-07-04 15:47:24 +0530

    [1438] [Time Beats: 54.72%] [Memory Beats: 96.18%] - Leet2Hub

--- SlidingWindow/MontonicDeque/1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit/1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.java
+++ SlidingWindow/MontonicDeque/1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit/1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.java
@@ -1,26 +1,41 @@
 1class Solution {
 2    public int longestSubarray(int[] nums, int limit) {
-3    TreeMap<Integer, Integer> map = new TreeMap<>();
-4      int max = 0, i =0, j =0, len = nums.length;
-5    while(j < len) {
-6        int end = nums[j];
-7      map.put(end, map.getOrDefault(end, 0)+1);
-8      if(map.lastKey()-map.firstKey()<=limit) {
-9        max = Math.max(max, j-i+1);
-10        j++;
-11      }
-12      else {
-13        while(i<=j && map.lastKey()-map.firstKey()>limit) {
-14            int start = nums[i];
-15            map.put(start, map.get(start)-1);
-16            if(map.get(start)==0){
-17                map.remove(start);
-18            }
-19            i++;
-20        }
-21        j++;
-22      }
-23    }
-24    return max;
-25    }
-26}
+3    Deque<Integer> maxDeque = new ArrayDeque<>();
+4    Deque<Integer> minDeque = new ArrayDeque<>();
+5    int i =0, max = 0;
+6    for(int j =0; j <nums.length;j++) {
+7        
+8        //remove indices whose values are smaller than current, we need maxEle
+9        while(!maxDeque.isEmpty() && nums[maxDeque.peekLast()]<=nums[j]){
+10            maxDeque.pollLast();
+11        }
+12        //add current at last , front will have currentMax
+13         maxDeque.offerLast(j);
+14
+15        //remove indices whose values are greater than current, we need minEle
+16        while(!minDeque.isEmpty() && nums[minDeque.peekLast()]>=nums[j]){
+17            minDeque.pollLast();
+18        }
+19         //add currentMax at Last,front will have currentMin
+20        minDeque.offerLast(j);
+21
+22        //while max-min>limit
+23        while(nums[maxDeque.peekFirst()] - nums[minDeque.peekFirst()] >limit)
+24         {
+25            //remove data related to i and slide window
+26            if(maxDeque.peekFirst()==i){
+27                maxDeque.pollFirst();
+28            }
+29            if(minDeque.peekFirst()==i){
+30                minDeque.pollFirst();
+31            }
+32              //move towards right until condition is satisfied
+33            i++;
+34        }
+35       max = Math.max(max, j-i+1);
+36    }
+37    return max;
+38  }
+39 }
+40
+41 // refer sliding window maximum problem
